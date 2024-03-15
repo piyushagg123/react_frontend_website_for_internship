5321d44eb97bb609dc569a5efcb0e8a6819abb19
Piyush <piyushaggarwal20903@gmail.com>
2024-03-15 15:10:05 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/Footer';
-import About from './components/About';
-import Home from './components/Home';
-import Blog from './components/Blog/Blog';
-import Profile from './components/Profile';
-import {BrowserRouter,Route,Routes,Link} from 'react-router-dom';
-import Login from './components/Log';
-import Sessions from './components/Navbar';
-import Search from './components/Search';
-import Session from './components/Sessions';
-import Workshop from './components/Workshops';
-import C_work from './components/C_work';
-import About_page from './components/About_page';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Home from "./components/Home";
+import Blog from "./components/Blog/Blog";
+import Profile from "./components/Profile";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Login from "./components/Log";
+import Search from "./components/Search";
+import Session from "./components/Sessions";
+import Workshop from "./components/Workshops";
+import C_work from "./components/C_work";
+import About_page from "./components/About_page";
 function App() {
   return (
     <div>
-     <BrowserRouter>
-     <Routes>
-        <Route path='/' element={<Home/>}/>
-        
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-        <Route path='/about' element={<About/>}/>
-        <Route path='/blog' element={<Blog/>}/>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/sessions' element={<Sessions/>}/>
-        <Route path='/search' element={<Search/>}/>
-        <Route path='/workshops' element={<Workshop/>}/>
-        <Route path='/session' element={<Session/>}/>
-        <Route path='/c-workshops' element={<C_work/>}/>
-        <Route path='/about_page' element={<About_page/>}/>
-
-      </Routes>
-     </BrowserRouter>
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/workshops" element={<Workshop />} />
+          <Route path="/session" element={<Session />} />
+          <Route path="/c-workshops" element={<C_work />} />
+          <Route path="/about_page" element={<About_page />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/NewNavBar.js
+++ src/components/NewNavBar.js
@@ -1,91 +1,204 @@
-import React,{useState} from 'react'
-import {NavLink,Link} from 'react-router-dom'
-import {FiAlignRight,FiXCircle,FiChevronDown} from 'react-icons/fi'
-import img from './Blog/images/header.jpg';
-import './New.css';
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { FiAlignRight, FiXCircle, FiChevronDown } from "react-icons/fi";
+import img from "./Blog/images/header.jpg";
+import "./New.css";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const NewNavBar = () => {
-    const { logout } = useAuth0();
-    const { loginWithRedirect,isAuthenticated } = useAuth0();
-    const [isMenu, setisMenu] = useState(false);
-    const [isResponsiveclose, setResponsiveclose] = useState(false);
-    const toggleClass = () => {
-      setisMenu(isMenu === false ? true : false);
-      setResponsiveclose(isResponsiveclose === false ? true : false);
+  const { logout } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const [isMenu, setisMenu] = useState(false);
+  const [isResponsiveclose, setResponsiveclose] = useState(false);
+  const toggleClass = () => {
+    setisMenu(isMenu === false ? true : false);
+    setResponsiveclose(isResponsiveclose === false ? true : false);
   };
-    let boxClass = ["main-menu menu-right menuq1"];
-    if(isMenu) {
-        boxClass.push('menuq2');
-    }else{
-        boxClass.push('');
-    }
-    const [isMenuSubMenu, setMenuSubMenu] = useState(false);
-    const toggleSubmenu = () => {
-      setMenuSubMenu(isMenuSubMenu === false ? true : false);
-    };
-    let boxClassSubMenu = ["sub__menus"];
-    if(isMenuSubMenu) {
-        boxClassSubMenu.push('sub__menus__Active');
-    }else {
-        boxClassSubMenu.push('');
-    }
+  let boxClass = ["main-menu menu-right menuq1"];
+  if (isMenu) {
+    boxClass.push("menuq2");
+  } else {
+    boxClass.push("");
+  }
+  const [isMenuSubMenu, setMenuSubMenu] = useState(false);
+  const toggleSubmenu = () => {
+    setMenuSubMenu(isMenuSubMenu === false ? true : false);
+  };
+  let boxClassSubMenu = ["sub__menus"];
+  if (isMenuSubMenu) {
+    boxClassSubMenu.push("sub__menus__Active");
+  } else {
+    boxClassSubMenu.push("");
+  }
   return (
-    <div id='sd'>
-        <header className="header__middle">
+    <div
+      id="sd"
+      style={{ background: "linear-gradient(90deg, #90ff90, #e6ffe6)" }}
+    >
+      <header className="header__middle">
         <div className="container">
-            <div className="row">
-                {/* Add Logo  */}
-                <div className="header__middle__logo">
-                    <NavLink exact activeClassName='is-active' to="/">
-                        <img  src={img} alt="logo" /> 
-                    </NavLink>
-                </div>
-                <div className="header__middle__menus">
-                    <nav className="main-nav " >
-                    {/* Responsive Menu Button */}
-                    {isResponsiveclose === true ? <> 
-                        <span className="menubar__button" style={{ display: 'none' }} onClick={toggleClass} > <FiXCircle />   </span>
-                    </> : <> 
-                        <span className="menubar__button" style={{ display: 'none' }} onClick={toggleClass} > <FiAlignRight />   </span>
-                    </>}
-                    <ul className={boxClass.join(' ')}>
-                        <li  className="menu-item" >
-                            <NavLink exact activeClassName='is-active' onClick={toggleClass} to={`/`}> Home </NavLink> 
-                        </li>
-                        <li className="menu-item " ><NavLink onClick={toggleClass} activeClassName='is-active' to={`/about_page`}> About Us</NavLink> </li>
-                        <li onClick={toggleSubmenu} className="menu-item sub__menus__arrows" > <Link to="#"> Yoga Sessions <FiChevronDown /> </Link>
-                            <ul className={boxClassSubMenu.join(' ')} > 
-                                <li> <NavLink onClick={toggleClass} activeClassName='is-active'  to={`/session`}> Sessions </NavLink> </li>
-                                <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/workshops`}> Workshops </NavLink> </li>
-                                <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/c-workshops`}> Corporate Workshops </NavLink> </li>
-                            </ul>
-                        </li>
-                        <li className="menu-item " ><NavLink onClick={toggleClass} activeClassName='is-active' to={`/blog`}> Blog </NavLink> </li>
-                        {
-                            isAuthenticated ?
-                                <li className="menu-item " ><NavLink onClick={toggleClass} activeClassName='is-active' to={`/profile`}> Profile </NavLink> </li> :
-                                <li></li>
-                            
-                        }
-                        {
-                            isAuthenticated ? (<li className='menu-item signUp'><button className="sign btn btn-success" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-                            Log Out
-                          </button></li>):(
-                                <li className='menu-item signUp'><button className=" sign btn btn-success large" onClick={() => loginWithRedirect()}>Log In</button></li>
-                          )
-                        }
-                        
-                        
-                        
-                    </ul>
-                    </nav>     
-                </div>   
+          <div className="row" style={{ alignItems: "center" }}>
+            {/* Add Logo  */}
+            <div className="header__middle__logo">
+              <NavLink exact activeClassName="is-active" to="/">
+                <img src={img} alt="logo" />
+              </NavLink>
             </div>
-	    </div>
-    </header>
-    </div>
-  )
-}
+            <div className="header__middle__menus">
+              <nav className="main-nav ">
+                {/* Responsive Menu Button */}
+                {isResponsiveclose === true ? (
+                  <>
+                    <span
+                      className="menubar__button"
+                      style={{ display: "none" }}
+                      onClick={toggleClass}
+                    >
+                      {" "}
+                      <FiXCircle />{" "}
+                    </span>
+                  </>
+                ) : (
+                  <>
+                    <span
+                      className="menubar__button"
+                      style={{ display: "none" }}
+                      onClick={toggleClass}
+                    >
+                      {" "}
+                      <FiAlignRight />{" "}
+                    </span>
+                  </>
+                )}
+                <ul className={boxClass.join(" ")}>
+                  <li className="menu-item">
+                    <NavLink
+                      exact
+                      activeClassName="is-active"
+                      onClick={toggleClass}
+                      to={`/`}
+                    >
+                      {" "}
+                      Home{" "}
+                    </NavLink>
+                  </li>
+                  <li className="menu-item ">
+                    <NavLink
+                      onClick={toggleClass}
+                      activeClassName="is-active"
+                      to={`/about_page`}
+                    >
+                      {" "}
+                      About Us
+                    </NavLink>{" "}
+                  </li>
+                  <li
+                    onClick={toggleSubmenu}
+                    className="menu-item sub__menus__arrows"
+                  >
+                    {" "}
+                    <Link to="#">
+                      {" "}
+                      Yoga Sessions <FiChevronDown />{" "}
+                    </Link>
+                    <ul className={boxClassSubMenu.join(" ")}>
+                      <li>
+                        {" "}
+                        <NavLink
+                          onClick={toggleClass}
+                          activeClassName="is-active"
+                          to={`/session`}
+                        >
+                          {" "}
+                          Sessions{" "}
+                        </NavLink>{" "}
+                      </li>
+                      <li>
+                        <NavLink
+                          onClick={toggleClass}
+                          activeClassName="is-active"
+                          to={`/workshops`}
+                        >
+                          {" "}
+                          Workshops{" "}
+                        </NavLink>{" "}
+                      </li>
+                      <li>
+                        <NavLink
+                          onClick={toggleClass}
+                          activeClassName="is-active"
+                          to={`/c-workshops`}
+                        >
+                          {" "}
+                          Corporate Workshops{" "}
+                        </NavLink>{" "}
+                      </li>
+                    </ul>
+                  </li>
+                  <li className="menu-item ">
+                    <NavLink
+                      onClick={toggleClass}
+                      activeClassName="is-active"
+                      to={`/blog`}
+                    >
+                      {" "}
+                      Blog{" "}
+                    </NavLink>{" "}
+                  </li>
+                  {isAuthenticated ? (
+                    <li className="menu-item ">
+                      <NavLink
+                        onClick={toggleClass}
+                        activeClassName="is-active"
+                        to={`/profile`}
+                      >
+                        {" "}
+                        Profile{" "}
+                      </NavLink>{" "}
+                    </li>
+                  ) : (
+                    <li></li>
+                  )}
+                  {isAuthenticated ? (
+                    <li className="menu-item signUp">
+                      <button
+                        className="sign btn btn-success"
+                        onClick={() =>
+                          logout({
+                            logoutParams: { returnTo: window.location.origin },
+                          })
+                        }
+                      >
+                        Log Out
+                      </button>
+                    </li>
+                  ) : (
+                    <li className="menu-item signUp">
+                      <button
+                        className=" sign btn btn-success large"
+                        onClick={() => loginWithRedirect()}
+                      >
+                        Log In
+                      </button>
+                    </li>
+                  )}
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
-export default NewNavBar
+      <br />
+    </div>
+  );
+};
+
+export default NewNavBar;
